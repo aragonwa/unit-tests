b00c48cc4fb2dfcbd56662f4a14b1da689c33e21
Alex Aragon <Alex.Aragon@kingcounty.gov>
2017-04-06 16:58:34 -0700

    Inital push

---
+++ test/moreTests.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// jshist expr: true
+
+var chai = require('chai');
+var expect = chai.expect;
+
+chai.should();
+
+describe('some other tests', function () {
+  it('should be true', function () {
+    expect(true).to.be.true;
+  });
+
+});
